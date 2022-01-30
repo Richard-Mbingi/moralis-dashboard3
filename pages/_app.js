a1755caf701113abc6971a7fc01ff10f13266ce1
Richard Mbingi <64406225+Richard-Mbingi@users.noreply.github.com>
2022-01-30 09:57:13 +0300

    Metamask Authentication

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <ChakraProvider>
       <MoralisProvider
         appId={process.env.NEXT_PUBLIC_APP_ID}
-        serverUrl={process.env.NEXT_PUBLIC_SERVER_UR}
+        serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
       >
         <Component {...pageProps} />
       </MoralisProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
+import { Flex, Text, Button } from "@chakra-ui/react";
+import Head from "next/head";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
 
 export default function Home() {
-  const {} = useMoralis()
+  const { isAuthenticated, authenticate } = useMoralis();
+  if (!isAuthenticated) {
+    return (
+      <>
+        <Head>
+          <title>Login | Dashboard3</title>
+        </Head>
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          width="100vw"
+          height="100vh"
+          bgGradient="linear(to-br, teal.400, purple.300)"
+        >
+          <Text fontSize="5xl" fontWeight="bold" color="white">
+            Dashboard3
+          </Text>
+          <Button
+            colorScheme="purple"
+            size="lg"
+            mt="6"
+            onClick={() => authenticate({})}
+          >
+            Login with MetaMask
+          </Button>
+        </Flex>
+      </>
+    );
+  }
   return (
     <div>
-      <h1>Hello world</h1>
+      <Head>
+        <title>Dashboard3</title>
+      </Head>
+      <Flex>
+        <Header />
+      </Flex>
     </div>
   );
 }
